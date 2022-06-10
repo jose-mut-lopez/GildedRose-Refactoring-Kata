40c1bcae66a22293db3025b9790eb7e15114f71b
jose miguel mut lopez <jose.mut@intrasonics.com>
2022-06-10 17:21:10 +0100

    extract backstage passes

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -40,7 +40,19 @@ impl GildedRose {
     }
 
     fn update_quality_single_item(item: &mut Item) {
-        if item.name == SULFURAS {} else {
+        if item.name == SULFURAS {
+        } else if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+            if item.sell_in <= 10 {
+                item.quality += 2;
+            }
+            if item.sell_in <= 5 {
+                item.quality += 1;
+            }
+            if item.sell_in <= 0 {
+                item.quality = 0;
+            }
+            item.sell_in -= 1;
+        } else {
             if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
             {
                 if item.quality > 0 {
