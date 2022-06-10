62f8fd1f0cdb52ecd28e5564e6f2d274ee98dab6
jose miguel mut lopez <jose.mut@intrasonics.com>
2022-06-10 17:25:33 +0100

    replaced backstag passes

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -51,30 +51,18 @@ impl GildedRose {
             if item.sell_in <= 0 {
                 item.quality = 0;
             }
+            if item.quality > 50 {
+                item.quality = 50;
+            }
             item.sell_in -= 1;
         } else {
-            if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-            {
+            if item.name != "Aged Brie" {
                 if item.quality > 0 {
                     item.quality = item.quality - 1;
                 }
             } else {
                 if item.quality < 50 {
                     item.quality = item.quality + 1;
-
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.sell_in < 11 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if item.sell_in < 6 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
                 }
             }
 
@@ -82,13 +70,9 @@ impl GildedRose {
 
             if item.sell_in < 0 {
                 if item.name != "Aged Brie" {
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
                         if item.quality > 0 {
                             item.quality = item.quality - 1;
                         }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
                 } else {
                     if item.quality < 50 {
                         item.quality = item.quality + 1;
@@ -143,6 +127,16 @@ mod tests {
         let items = vec![Item::new("Aged Brie", 1, 10)];
         let mut rose = GildedRose::new(items);
         for _i in 0..120 {
+            rose.update_quality();
+        }
+
+        assert_eq!(rose.items[0].quality, 50);
+    }
+    #[test]
+    fn test_pass_does_not_increase_over_50() {
+        let items = vec![Item::new("Backstage passes to a TAFKAL80ETC concert", 10, 45)];
+        let mut rose = GildedRose::new(items);
+        for _i in 0..8 {
             rose.update_quality();
         }
 
