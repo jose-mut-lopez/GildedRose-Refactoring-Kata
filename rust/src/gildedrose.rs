ed143cd934c98e2ca3c07b1e60a9f42d710263c6
jose miguel mut lopez <jose.mut@intrasonics.com>
2022-06-10 17:30:10 +0100

    remove old brie code

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -66,27 +66,15 @@ impl GildedRose {
                 }
             }
         } else {
-            if item.name != BRIE {
-                if item.quality > 0 {
-                    item.quality = item.quality - 1;
-                }
-            } else {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1;
-                }
+            if item.quality > 0 {
+                item.quality = item.quality - 1;
             }
 
             item.sell_in = item.sell_in - 1;
 
             if item.sell_in < 0 {
-                if item.name != BRIE {
-                    if item.quality > 0 {
-                        item.quality = item.quality - 1;
-                    }
-                } else {
-                    if item.quality < 50 {
-                        item.quality = item.quality + 1;
-                    }
+                if item.quality > 0 {
+                    item.quality = item.quality - 1;
                 }
             }
         }
